925980f1a1e614be19562b9440871c7d13fbb48e
Samantha Meyer <slmeyer90@gmail.com>
2022-05-26 13:33:41 -0400

    roman numerals helper reworked for clarity

--- 4kyu/roman_numerals_helper/js/main.js
+++ 4kyu/roman_numerals_helper/js/main.js
@@ -22,65 +22,51 @@
 //   M	    1000
 
 const RomanNumerals = {
-    specRegex: [
-    [/CM/,900],[/CD/,400],[/XC/,90],[/XL/,40],[/IX/,9],[/IV/,4]],
-    regRegex: [
-    [/M+/,1000],[/D+/,500],[/C+/,100],[/L+/,50],[/X+/,10],[/V+/,5],[/I+/,1]],
-
-    basicRoman: [
-    ['M',1000],['C',100],['X',10],['I',1]],
-    specRoman: [
-    [/C{9}/,'CM'],[/C{5}/,'D'],[/C{4}/,'CD'],
-    [/X{9}/,'XC'],[/X{5}/,'L'],[/X{4}/,'XL'],
-    [/I{9}/,'IX'],[/I{5}/,'V'],[/I{4}/,'IV']],
+    'M':  1000,
+    'CM': 900,
+    'D':  500,
+    'CD': 400,
+    'C':  100,
+    'XC': 90,
+    'L':  50,
+    'XL': 40,
+    'X':  10,
+    'IX': 9,
+    'V':  5,
+    'IV': 4,
+    'I':  1,
 
     toRoman: function(decimal) {
         let result = '';
-        this.basicRoman.forEach(set => {
-            while (decimal >= set[1]) {
-                result += set[0];
-                decimal -= set[1];
+        for (let key in this) {
+            while (decimal >= this[key]) {
+                result += key;
+                decimal -= this[key];
             }
-        })
-        this.specRoman.forEach(set => {
-            result = result.replace(set[0],set[1]);
-        })
+        }
         return result;
     },
 
-    fromRoman: function(roman) {
+    fromRoman: function(roNum) {
         let result = 0;
-        this.specRegex.forEach(reg => {
-            if (reg[0].test(roman)) {
-                roman = roman.replace(reg[0],'');
-                result += reg[1];
+        let start = 0;
+        for (let key in this) {
+            while (roNum.startsWith(key,start)) {
+                result += this[key];
+                start += key.length;
             }
-        })
-        this.regRegex.forEach(regex => {
-            let matches = roman.match(regex[0]);
-            if (matches) {
-                result += regex[1] * matches[0].length;
-            }
-        })
+        }
         return result;
     }
 }
 
-// let test = 'XC'
-// (test.match(/CM/));   // 900
-// (test.match(/CD/));   // 400
-// (test.match(/XC/));   // 90
-// (test.match(/XL/));   // 40
-// (test.match(/IX/));   // 9
-// (test.match(/IV/));   // 4
+console.log(RomanNumerals.toRoman(2466));   // MMCDLXVI
+console.log(RomanNumerals.toRoman(3999));   // MMMCMXCIX
+console.log(RomanNumerals.toRoman(10));     // X
+console.log(RomanNumerals.toRoman(3504));   // MMMDIV
 
-// if (test.match(/XL/)) {
-//     console.log('yes')
-// }
-
-console.log(RomanNumerals.toRoman(2466));
-console.log(RomanNumerals.toRoman(3999));
-console.log(RomanNumerals.toRoman(10));
-console.log(RomanNumerals.toRoman(3500));
-
-console.log(RomanNumerals.fromRoman('III'));
+console.log(RomanNumerals.fromRoman('III'));        // 3
+console.log(RomanNumerals.fromRoman('MMCDLXVI'));   // 2466
+console.log(RomanNumerals.fromRoman('MMMCMXCIX'));  // 3999
+console.log(RomanNumerals.fromRoman('X'));          // 10
+console.log(RomanNumerals.fromRoman('MMMDIV'));     // 3504
